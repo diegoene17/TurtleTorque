281000d0daeb3c2384cdbc34a0dc421cc2d8dd2b
Valeria258 <vaeriaplascencia1@gmail.com>
2021-02-16 21:58:40 -0600

    Funciones WriteCurrent y ReadCurrent

--- src/turtlebot3/turtlebot3_torque_driver.cpp
+++ src/turtlebot3/turtlebot3_torque_driver.cpp
@@ -57,12 +57,15 @@ bool Turtlebot3TorqueDriver::init(String turtlebot3)
   setTorque(true);
 
   groupSyncWriteVelocity_ = new dynamixel::GroupSyncWrite(portHandler_, packetHandler_, ADDR_X_GOAL_VELOCITY, LEN_X_GOAL_VELOCITY);
+  groupSyncWriteCurrent_ = new dynamixel::GroupSyncWrite(portHandler_, packetHandler_, ADDR_X_GOAL_CURRENT, LEN_X_GOAL_CURRENT);
   groupSyncReadEncoder_   = new dynamixel::GroupSyncRead(portHandler_, packetHandler_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
+  groupSyncReadCurrent_ = new dynamixel::GroupSyncRead(portHandler_, packetHandler_, ADDR_X_PRESENT_CURRENT, LEN_X_PRESENT_CURRENT);
 
   if (turtlebot3 == "Burger")
     dynamixel_limit_max_velocity_ = BURGER_DXL_LIMIT_MAX_VELOCITY;
   else if (turtlebot3 == "Waffle or Waffle Pi")
     dynamixel_limit_max_velocity_ = WAFFLE_DXL_LIMIT_MAX_VELOCITY;
+    dynamixel_limit_max_current_ = WAFFLE_DXL_LIMIT_MAX_CURRENT;
   else
     dynamixel_limit_max_velocity_ = BURGER_DXL_LIMIT_MAX_VELOCITY;
 
@@ -156,6 +159,44 @@ bool Turtlebot3TorqueDriver::readEncoder(int32_t &left_value, int32_t &right_val
   return true;
 }
 
+bool Turtlebot3TorqueDriver::readCurrent(int32_t &left_value, int32_t &right_value)
+{
+  int dxl_comm_result = COMM_TX_FAIL;              // Communication result
+  bool dxl_addparam_result = false;                // addParam result
+  bool dxl_getdata_result = false;                 // GetParam result
+
+  // Set parameter
+  dxl_addparam_result = groupSyncReadCurrent_->addParam(left_wheel_id_);
+  if (dxl_addparam_result != true)
+    return false;
+
+  dxl_addparam_result = groupSyncReadCurrent_->addParam(right_wheel_id_);
+  if (dxl_addparam_result != true)
+    return false;
+
+  // Syncread present position
+  dxl_comm_result = groupSyncReadCurrent_->txRxPacket();
+  if (dxl_comm_result != COMM_SUCCESS)
+    Serial.println(packetHandler_->getTxRxResult(dxl_comm_result));
+
+  // Check if groupSyncRead data of Dynamixels are available
+  dxl_getdata_result = groupSyncReadCurrent_->isAvailable(left_wheel_id_, ADDR_X_PRESENT_CURRENT, LEN_X_PRESENT_CURRENT);
+  if (dxl_getdata_result != true)
+    return false;
+
+  dxl_getdata_result = groupSyncReadCurrent_->isAvailable(right_wheel_id_, ADDR_X_PRESENT_CURRENT, LEN_X_PRESENT_CURRENT);
+  if (dxl_getdata_result != true)
+    return false;
+
+  // Get data
+  left_value  = groupSyncReadCurrent_->getData(left_wheel_id_,  ADDR_X_PRESENT_CURRENT, LEN_X_PRESENT_CURRENT);
+  right_value = groupSyncReadCurrent_->getData(right_wheel_id_, ADDR_X_PRESENT_CURRENT, LEN_X_PRESENT_CURRENT);
+
+  groupSyncReadCurrent_->clearParam();
+  return true;
+}
+
+
 bool Turtlebot3TorqueDriver::writeVelocity(int64_t left_value, int64_t right_value)
 {
   bool dxl_addparam_result;
@@ -191,6 +232,44 @@ bool Turtlebot3TorqueDriver::writeVelocity(int64_t left_value, int64_t right_val
   }
 
   groupSyncWriteVelocity_->clearParam();
+  return true;
+}
+
+bool Turtlebot3MotorDriver::writeCurrent(int64_t left_value, int64_t right_value)
+{
+  bool dxl_addparam_result;
+  int8_t dxl_comm_result;
+
+  uint8_t left_data_byte[4] = {0, };
+  uint8_t right_data_byte[4] = {0, };
+
+
+  left_data_byte[0] = DXL_LOBYTE(DXL_LOWORD(left_value));
+  left_data_byte[1] = DXL_HIBYTE(DXL_LOWORD(left_value));
+  left_data_byte[2] = DXL_LOBYTE(DXL_HIWORD(left_value));
+  left_data_byte[3] = DXL_HIBYTE(DXL_HIWORD(left_value));
+
+  dxl_addparam_result = groupSyncWriteCurrent_->addParam(left_wheel_id_, (uint8_t*)&left_data_byte);
+  if (dxl_addparam_result != true)
+    return false;
+
+  right_data_byte[0] = DXL_LOBYTE(DXL_LOWORD(right_value));
+  right_data_byte[1] = DXL_HIBYTE(DXL_LOWORD(right_value));
+  right_data_byte[2] = DXL_LOBYTE(DXL_HIWORD(right_value));
+  right_data_byte[3] = DXL_HIBYTE(DXL_HIWORD(right_value));
+
+  dxl_addparam_result = groupSyncWriteCurrent_->addParam(right_wheel_id_, (uint8_t*)&right_data_byte);
+  if (dxl_addparam_result != true)
+    return false;
+
+  dxl_comm_result = groupSyncWriteCurrent_->txPacket();
+  if (dxl_comm_result != COMM_SUCCESS)
+  {
+    Serial.println(packetHandler_->getTxRxResult(dxl_comm_result));
+    return false;
+  }
+
+  groupSyncWriteCurrent_->clearParam();
   return true;
 }
 
