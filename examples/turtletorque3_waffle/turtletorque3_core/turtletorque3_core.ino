6c6d3311fd61eed9d79a32945459ad06fa22fe7d
Valeria258 <vaeriaplascencia1@gmail.com>
2021-02-22 18:59:53 -0600

    respuesta a idea

--- examples/turtletorque3_waffle/turtletorque3_core/turtletorque3_core.ino
+++ examples/turtletorque3_waffle/turtletorque3_core/turtletorque3_core.ino
@@ -92,6 +92,7 @@ void loop()
   if ((t-tTime[1]) >= (1000 / CMD_VEL_PUBLISH_FREQUENCY))
   {
     //Podriamos matar el control con el mando y meter aqui el publishCmdTor
+    //SIMON
     publishCmdVelFromRC100Msg();
     tTime[1] = t;
   }
