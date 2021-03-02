a4fa13ac8d3bf35c3cd6a0665e8a55b3afe6558d
Valeria258 <vaeriaplascencia1@gmail.com>
2021-03-01 21:25:58 -0600

    .

--- examples/turtletorque3_waffle/turtletorque3_core/turtletorque3_core.ino
+++ examples/turtletorque3_waffle/turtletorque3_core/turtletorque3_core.ino
@@ -184,7 +184,7 @@ void commandVelocityCallback(const geometry_msgs::Twist& cmd_vel_msg)
 */
 void commandTorqueCallback(const turtlebot3_msgs::WrenchArray& cmd_tor_msg)
 {
-  const geometry_msgs::Wrench &lefth_value = cmd_tor_msg.wrenches[LEFT];
+  const geometry_msgs::Wrench &left_value = cmd_tor_msg.wrenches[LEFT];
   const geometry_msgs::Wrench &right_value = cmd_tor_msg.wrenches[RIGHT];
 
 
