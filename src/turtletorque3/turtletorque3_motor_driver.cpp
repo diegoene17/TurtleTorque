fe2df63ae950a94d79120290a8a1f9610d3680fa
diegoene17 <diegoalejandroene17@gmail.com>
2021-03-01 21:19:47 -0600

    .

--- src/turtletorque3/turtletorque3_motor_driver.cpp
+++ src/turtletorque3/turtletorque3_motor_driver.cpp
@@ -237,11 +237,9 @@ bool TurtleTorque3MotorDriver::writeCurrent(int64_t left_value, int64_t right_va
 bool TurtleTorque3MotorDriver::controlMotor(int64_t left_value, int64_t right_value)
 {
   bool dxl_comm_result = false;
-  int64_t left_current_value = 0;
-  int64_t right_current_value = 0;
   //Aqui iria el controlador en caso de ser necesario
 
-  dxl_comm_result = writeCurrent(left_current_value, right_current_value);
+  dxl_comm_result = writeCurrent(left_value right_value);
   if(dxl_comm_result == false)
     return false;
   return true;
