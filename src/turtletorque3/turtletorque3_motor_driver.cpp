0736259203afaff371c31e73fb5bac24acc23340
Valeria <valeriaplascencia1@gmail.com>
2021-03-16 20:02:59 -0600

    Cambios al imprimir en pruebas

--- src/turtletorque3/turtletorque3_motor_driver.cpp
+++ src/turtletorque3/turtletorque3_motor_driver.cpp
@@ -209,26 +209,26 @@ bool TurtleTorque3MotorDriver::writeCurrent(int32_t left_value, int32_t right_va
   left_data_byte[0] = DXL_LOBYTE(DXL_LOWORD(left_value));
   left_data_byte[1] = DXL_HIBYTE(DXL_LOWORD(left_value));
 
-  DEBUG.println("Valor izquiero: %i %i",left_data_byte[1],left_data_byte[0]);
+  DEBUG_SERIAL.println("Valor izquiero: %i %i",left_data_byte[1],left_data_byte[0]);
 
   dxl_addparam_result = groupSyncWriteCurrent_->addParam(left_wheel_id_, (uint8_t*)&left_data_byte);
   //dxl_addparam_result = groupSyncCurrentLimit_->addParam(left_wheel_id_, (uint8_t*)&left_data_byte);
 
   if (dxl_addparam_result != true){
     //Borrar impresión cuando comprobemos
-    DEBUG.println("Fallo motor izquierdo");
+    DEBUG_SERIAL.println("Fallo motor izquierdo");
     return false;
   }
 
   right_data_byte[0] = DXL_LOBYTE(DXL_LOWORD(right_value));
   right_data_byte[1] = DXL_HIBYTE(DXL_LOWORD(right_value));
-  DEBUG.println("Valor derecho: %i %i",right_data_byte[1],right_data_byte[0]);
+  DEBUG_SERIAL.println("Valor derecho: %i %i",right_data_byte[1],right_data_byte[0]);
 
   dxl_addparam_result = groupSyncWriteCurrent_->addParam(right_wheel_id_, (uint8_t*)&right_data_byte);
   //dxl_addparam_result = groupSyncCurrentLimit_->addParam(left_wheel_id_, (uint8_t*)&left_data_byte);
   if (dxl_addparam_result != true){
     //Borrar impresión cuando comprobemos
-    DEBUG.println("Fallo motor derecho");
+    DEBUG_SERIAL.println("Fallo motor derecho");
     return false;
   }
 
